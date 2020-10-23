d9237a1407ed4277f44e68701af6229183eb0ecf
EC2 Default User <ec2-user@ip-172-31-69-152.ec2.internal>
2020-10-23 06:02:18 +0000

    Add controller (user,post,question)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  get 'questions/index'
+  get 'questions/show'
+  get 'questions/edit'
+  get 'questions/new'
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/edit'
+  get 'posts/new'
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
