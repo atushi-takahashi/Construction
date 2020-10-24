e3c3b9e08273ee5f614999907d25d790fbce94c1
EC2 Default User <ec2-user@ip-172-31-69-152.ec2.internal>
2020-10-24 16:54:52 +0000

    update [guest-login,header]

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,16 @@
+class Users::SessionsController < Devise::SessionsController
+  def new_guest
+    user = User.find_or_create_by(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲストユーザー"
+      user.job_years = "4"
+      user.profile = "神奈川県の地場ゼネコンで土木の施工管理をしています。
+                      最近は仕事にも慣れてきてできる仕事の幅も広がったの
+                      ですが、その分わからないことも増えてきたので質問、
+                      また共有させていただきたいと思いこのサイトに登録しました。"
+      user.occupation = "施工管理（土木"
+    end
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root to: 'welcome#top'
   get 'welcome/about' => 'welcome#about'
   devise_for :users
+  devise_scope :user do
+    post '/users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   resources :users, only: [:show, :index, :edit, :update]
   resources :posts
   resources :questions
