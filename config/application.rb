6c607c9c246ef27b51637375292747b7ec2aa1cc
EC2 Default User <ec2-user@ip-172-31-69-152.ec2.internal>
2020-10-25 08:59:58 +0000

    Update[error english=>japanese]

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module ConstructionBlog
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
     config.time_zone = 'Asia/Tokyo'
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', 'models', '*ja.yml').to_s]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
